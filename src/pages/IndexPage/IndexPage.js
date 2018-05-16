f3609cd806ddbd99258948c4157aa27028c5a922
dicksonp <dicksonp@seekasia.com>
2018-05-16 11:39:42 +0800

    - JobDescription - AdditionalOptions

--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -5,12 +5,16 @@ import { withRouter } from 'react-router-dom';
 
 import JobDetails from '../../components/JobDetails/JobDetails';
 import JobRequirements from '../../components/JobRequirements/JobRequirements';
+import JobDescription from '../../components/JobDescription/JobDescription';
+import AdditionalOptions from '../../AdditionalOptions/AdditionalOptions';``
 
 const IndexPage = () => {
 	return (
 		<div className={styles.container}>
 			<JobDetails />
 			<JobRequirements />
+			<JobDescription />
+			<AdditionalOptions />
 		</div>
 	);
 };
